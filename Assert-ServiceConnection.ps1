f6ad37b3c8c7a87b4248699bcbb38ff04473733d
= <=>
2021-01-14 13:02:14 -0300

    pushing changes from 01/14

---
+++ Assert-ServiceConnection.ps1
@@ -0,0 +1,34 @@
+﻿function Assert-ServiceConnection {
+    <#
+    .SYNOPSIS
+    Checks current connection status for SCC, EXO and AzureAD
+    
+    .DESCRIPTION
+    Checks current connection status for SCC, EXO and AzureAD
+    
+    .EXAMPLE
+    PS C:\> Assert-ServiceConnection
+    Checks current connection status for SCC, EXO and AzureAD
+    #>
+    [CmdletBinding()]
+    param (
+        # Parameters
+    )
+    $Sessions = Get-PSSession
+    $ServicesToConnect = New-Object -TypeName "System.Collections.ArrayList"
+
+    # Check if SCC connection
+    if ( -not ($Sessions.ComputerName -match "ps.compliance.protection.outlook.com") ) { $null = $ServicesToConnect.add("SCC") }
+
+    # Check if EXO connection
+    if ( $Sessions.ComputerName -notcontains "outlook.office365.com" ) { $null = $ServicesToConnect.add("EXO") }
+
+    # Check if MSOnline connection
+    try{
+        $Null = Get-MsolCompanyInformation -ErrorAction Stop
+    }
+    catch {
+        $null = $ServicesToConnect.add("MSOnline")
+    }
+    return $ServicesToConnect
+}
---
+++ Changelog.md
@@ -0,0 +1,8 @@
+# Change log  
+
+## 01/14/2020
+ - update: change connection logic to the beginning of the script, and remove multiple connection assertions.
+ - update: corrected a few grammar and indention on the script.
+ - added: Display disclaimer to the host.
+ - added: Assertion functions to handle the connection logic.
+ - fixed: compliancesearch name in option 2 was incorrectly hard-coded to a test name.
